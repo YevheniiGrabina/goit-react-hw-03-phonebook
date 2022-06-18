4a7f445a014f3e7ab5c03dd4a8429d58612eeee6
YevheniiGrabina <89870811+YevheniiGrabina@users.noreply.github.com>
2022-06-18 19:52:00 +0300

    add localStor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,6 +11,19 @@ class App extends Component {
     filter: '',
   };
 
+    componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   
   handleFilterChange = e => {
     const { name, value } = e.currentTarget;
